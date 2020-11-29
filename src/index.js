1588f959ccf0f5002e80910b39e1ac216f1b2225
KerstiA <74317766+KerstiA@users.noreply.github.com>
2020-11-29 22:19:45 +0200

    added current time

--- src/index.js
+++ src/index.js
@@ -1,21 +1,54 @@
+function formatDate(timestamp) {
+  let currentDay = new Date(timestamp);
+  let hours = currentDay.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = currentDay.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let date = currentDay.getDate();
+  if (date < 10) {
+    date = `0${date}`;
+  }
+  let year = currentDay.getFullYear();
+  let months = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sept",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+  let month = months[currentDay.getMonth()];
+  return `${date} /${month} /${year}  ${hours}:${minutes}`;
+}
 function currentTemp(response) {
-    console.log(response.data);
+  console.log(response.data);
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
-  let precipitationElement = document.querySelector("#precipitation");
+  let iconElement = document.querySelector("#weather-icon");
+  let dateElement = document.querySelector("#date");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
-windElement.innerHTML = Math.round(response.data.wind.speed);
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+  iconElement.innerHTML = response.data.weather[0].icon;
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
-
-
 
 let apiKey = `88493d926515e36fa055dfe27bbb8ecd`;
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Tallinn&appid=${apiKey}&units=metric`;
-  
+
 axios.get(apiUrl).then(currentTemp);
